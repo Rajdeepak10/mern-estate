52c5fb19069bf107608d2f0fa5cf7d8e3fb492f8
Rajdeepak <rajdeepakchauhan9@gmail.com>
2024-01-04 18:56:27 +0530

    user model created

---
+++ api/Models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+// creation of user schema
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    email:{
+        type: String,
+        unique: true,
+        required: true
+
+    },
+    password:{
+        type: String,
+        required: true
+    }
+},{timestamps:true});
+// creation of user model
+const User = mongoose.model('User',userSchema)
+export default User;
