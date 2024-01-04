7dd35f8ef7c5af24a023d11d938a9b8cdf4ee7d6
Rajdeepak <rajdeepakchauhan9@gmail.com>
2024-01-04 19:55:07 +0530

    signup functionality

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRouter from './Routes/user.route.js'
+import authRouter from './Routes/auth.route.js'
 dotenv.config()
 
 
@@ -14,9 +15,15 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 const app = express()
 
+
+// by default we are not allowed to send json as a input so we use this 
+app.use(express.json())
+
+
 app.listen(3000,()=>{
     console.log("server is running at port 3000!!")
 })
 
 // api routes
 app.use('/api/user',userRouter)
+app.use('/api/auth',authRouter)
